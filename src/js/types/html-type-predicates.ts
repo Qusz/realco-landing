df47d9538427b7423542d65d45690f5316f9bc5f
Qusz <de.gorchakov@gmail.com>
2023-03-27 19:26:10 +0300

    adds type predicates

---
+++ src/js/types/html-type-predicates.ts
@@ -0,0 +1,47 @@
+function isHTMLElement(node: HTMLElement | null): node is HTMLElement {
+  return node !== null;
+}
+
+function isHTMLDivElement(node: HTMLDivElement | null): node is HTMLDivElement {
+  return node !== null;
+}
+
+function isHTMLInputElement(
+  node: HTMLInputElement | null
+): node is HTMLInputElement {
+  return node !== null;
+}
+
+function isHTMLUListElement(
+  node: HTMLUListElement | null
+): node is HTMLUListElement {
+  return node !== null;
+}
+
+function isHTMLParagraphElement(
+  node: HTMLParagraphElement | null
+): node is HTMLParagraphElement {
+  return node !== null;
+}
+
+function isHTMLSpanElement(
+  node: HTMLSpanElement | null
+): node is HTMLSpanElement {
+  return node !== null;
+}
+
+function isHTMLButtonElement(
+  node: HTMLButtonElement | null
+): node is HTMLButtonElement {
+  return node !== null;
+}
+
+export {
+  isHTMLElement,
+  isHTMLDivElement,
+  isHTMLInputElement,
+  isHTMLUListElement,
+  isHTMLParagraphElement,
+  isHTMLSpanElement,
+  isHTMLButtonElement
+};
