2d3d21ad747d23099635abbe3c076837c95dd37b
Qusz <de.gorchakov@gmail.com>
2023-04-15 09:37:28 +0300

    imports modules

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,2 +1,11 @@
 import 'normalize.css';
 import '@/scss/style.scss';
+import 'toastify-js/src/toastify.css';
+
+import navbar from './modules/navbar';
+import notification from './modules/notification';
+
+(() => {
+  navbar();
+  notification();
+})();
---
+++ src/js/types/index.ts
@@ -0,0 +1,3 @@
+export { GuaranteedElements } from './GuaranteedElements';
+export { NavbarElements } from './NavbarElements';
+export { NotificationElements } from './NotificationElements';
